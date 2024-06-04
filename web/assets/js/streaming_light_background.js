c5e98e33006485c5a7d410e02521d7de11ad4c7d
mengc <jsongkey@outlook.com>
2024-06-05 03:29:09 +0800

    添加了streaming-card

---
+++ web/assets/js/streaming_light_background.js
@@ -0,0 +1,8 @@
+window.onload = ()=>{
+    let cardIteam = document.getElementsByClassName("streaming-card");
+    console.log('cardIteam',cardIteam);
+    // cardIteam.forEach(element => {
+    //     console.log('element',element);
+        
+    // });
+}
